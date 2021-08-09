5d1e09a2a783b4d7d310149083389f8f07415957
glass1029 <glass1029.dev@gmail.com>
2021-08-09 14:12:45 +0900

    entries() 설명 추가

--- pages/object_prototype.js
+++ pages/object_prototype.js
@@ -9,7 +9,7 @@ Object prototype
 - 주어진 값이 있으면 값에 맞는 자료형의 객체를 반환함
 - 생성자가 아닌 맥락에서 호출하면 new Object()와 동일하게 동작함
 
-Object 생성자의 메서드 : assign(), create(), defineProperty(), defineProperties(), freeze(), getOwnPropertyDescriptor(), getOwnPropertyNames(), getOwnPropertySymbols(), getPrototypeOf(), is(), isExtensible(), isFrozen(), isSealed(), keys(), preventExtensions(), seal(), setPrototypeOf(), values()
+Object 생성자의 메서드 : assign(), create(), defineProperty(), entries(), defineProperties(), freeze(), getOwnPropertyDescriptor(), getOwnPropertyNames(), getOwnPropertySymbols(), getPrototypeOf(), is(), isExtensible(), isFrozen(), isSealed(), keys(), preventExtensions(), seal(), setPrototypeOf(), values()
 
 */
 
@@ -20,7 +20,7 @@ Object 생성자의 메서드 : assign(), create(), defineProperty(), defineProp
 const object1 = {
   a: 'something',
   b: 42,
-  c: false
+  c: false,
 };
 
 console.log(Object.keys(object1));  // ["a", "b", "c"]
@@ -30,3 +30,19 @@ console.log(Object.keys(object1));  // ["a", "b", "c"]
 // 주어진 객체의 열거가능한 모든 스트링 속성들의 값들을 포함하고 있는 배열을 반환
 
 console.log(Object.values(object1));  //["something" , 42, false]
+
+// 3. entries() ==============================
+// Object.entries()
+// 주어진 객체 자체의 enumerable 속성 [key, value] 쌍의 배열을 반환
+
+const object2 = {
+  a: 'something',
+  b: 42,
+};
+
+for (const [key, value] of Object.entries(object2)) {
+  console.log(`${key}: ${value}`);
+};
+
+// "a: something"
+// "b: 42"
