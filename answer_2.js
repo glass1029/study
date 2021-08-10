555c6b87562379a9d2d7c144cc3bdc217efbdc49
glass1029 <glass1029.dev@gmail.com>
2021-08-10 14:47:53 +0900

    3번 해답 추가

--- answer_2.js
+++ answer_2.js
@@ -40,18 +40,31 @@ const exam = {
 const titles = exam.data.movies.map(movie => movie.title);
 console.log(titles);
 
+
 // 2번: language 가 en 인 영화만 모아주세요!
+
 const enMoviesOnly = exam.data.movies.filter(movie => {
   return movie.language == "en";
 });
 console.log(enMoviesOnly);
 
+
 // 3번
-const compound = {
-  id : exam.data.movies.map(movie => movie.id),
-  title : exam.data.movies.map(movie => movie.title),
-  rating : exam.data.movies.map(movie => movie.rating),
-  language : exam.data.movies.map(movie => movie.language),
-};
+// const compound = {
+//   id : exam.data.movies.map(movie => movie.id),
+//   title : exam.data.movies.map(movie => movie.title),
+//   rating : exam.data.movies.map(movie => movie.rating),
+//   language : exam.data.movies.map(movie => movie.language),
+// };
+
+const targetArr = exam.data.movies  // 1.먼저 movies배열만 빼서 담기 {movie1[], movie2[], ...}
+const compound = targetArr.reduce((acc, current) => {  // reduce() 사용, accumulator에 저장, 배열 내 movie1[]부터 movie5[]까지 순차적으로 접근
+  Object.entries(current).forEach((entry) => {  // movie1[]의 첫번째 "id" : 32599 에 접근 후 아래 구현부 실행 -> "language" : "en" 까지 순차적으로 반복 접근
+    const [key, value] = entry; //const key = entry.key , const value = entry.value
+    if (!acc[key]) { acc[key] = [] }  //맨 처음 id, title, rating, language의 경우 초기값 빈배열 선언해줌
+    acc[key].push(value); //acc에 "id"인 key에 매칭된 value 값을 push
+  })
+  return acc  //reduce()의 accumulator 반환
+}, {})  //reduce()의 반환 형태 object로 선언
 
 console.log(compound);
